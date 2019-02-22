c192ddd44d81c20025013b666273e67c1d4b924c
Akaash Arora <37277266+akaasharora@users.noreply.github.com>
2019-02-21 19:31:26 -0500

    Add files via upload
    Files for Mr. JD

---
+++ BuyerUI/main.js
@@ -0,0 +1,40 @@
+// Get DOM Elements
+const modal = document.querySelector('#my-modal');
+const modalBtn = document.querySelector('#modal-btn');
+const closeBtn = document.querySelector('.close');
+const confirmBtn = document.querySelector('#confirm-btn');
+const cancelBtn = document.querySelector('#cancel-btn');
+
+
+// Events
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+confirmBtn.addEventListener('click', nextStep);
+cancelBtn.addEventListener('click', closeModal);
+
+
+// Open
+function openModal() {
+  modal.style.display = 'block';
+}
+
+// Close
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+// Close If Outside Click
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
+
+// next step if Confirm
+function nextStep(){
+    window.location.href='index2.html'
+    modal.style.display = 'block';
+    
+}
+
